fc93d778011323dce215875e106e63fda9a6aec2
ahasan-ullah <ahasanullah451@gmail.com>
2024-12-11 18:50:06 +0600

    lottie added

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,24 +1,30 @@
+import Lottie from "lottie-react";
+import registerLottieData from '../../assets/lottie/register.json'
 const Register = () => {
+  handleRegister=(e)=>{
+    e.preventDefault();
+    const form=e.target;
+    const email=form.email.value;
+    const pass=form.pass.value;
+
+    // pass validation
+  }
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+        <div className="text-center lg:text-left w-96">
+          <Lottie animationData={registerLottieData}></Lottie>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
         <h1 className="text-center text-5xl font-bold mt-5">Login now!</h1>
-          <form className="card-body">
+          <form onSubmit={handleRegister} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
                 type="email"
+                name="email"
                 placeholder="email"
                 className="input input-bordered"
                 required
@@ -30,6 +36,7 @@ const Register = () => {
               </label>
               <input
                 type="password"
+                name="pass"
                 placeholder="password"
                 className="input input-bordered"
                 required
